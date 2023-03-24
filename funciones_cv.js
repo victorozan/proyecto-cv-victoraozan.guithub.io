7d7d3f51a6ac99f6d6f5eb0c37110b71cc94afab
victorozan <victoraozan@gmail.com>
2023-03-24 12:50:28 -0300

    Initial commit

---
+++ funciones_cv.js
@@ -0,0 +1,10 @@
+
+document.getElementById('boton-css').onclick = function () {
+    document.getElementById('experiencia').innerHTML ="Trabajo con CSS «hojas de estilo en cascada» (Cascading Style Sheets) para el diseño web desde hace 4 años."
+}
+document.getElementById('boton-html').onclick = function () {
+    document.getElementById('experiencia').innerHTML ="Desarrollo HTML(‘lenguaje de marcado de hipertexto’), para la elaboración de páginas web desde hacer 4 años"
+}
+document.getElementById('boton-js').onclick = function () {
+    document.getElementById('experiencia').innerHTML ="Programo en JavaScript para hacer páginas web interactivas desde hace 3 años."
+}
